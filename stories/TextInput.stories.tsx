e0bea15ab9dfd500547f8bd82cbfef6cf6e00dc1
Eddie Lopez <ejlopez00@gmail.com>
2021-11-01 13:27:00 -0400

    component: created text input component
    Created the tet input component that will be used for all Apollo text
    intake needs.

---
+++ stories/TextInput.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { TextInput, Props } from '../src/components/TextInput/TextInput';
+
+const meta: Meta = {
+    title: 'Form/Text Input',
+    component: TextInput
+}
+
+export default meta;
+
+const Template: Story<Props> = (args) => <TextInput {...args}/>
+
+export const Default = Template.bind({});
