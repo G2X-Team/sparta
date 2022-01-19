d4f354cb52af06177a3729460c54fa642b3a0686
ahmed <ahmedreza80@gmail.com>
2022-01-19 21:18:38 +0530

    Added Loading State

---
+++ src/components/LoadingState/LoadingState.tsx
@@ -0,0 +1,155 @@
+import React, { useState, useEffect, HTMLAttributes, ReactNode } from 'react';
+import './LoadingState.css';
+
+export interface Props extends HTMLAttributes<HTMLDivElement> {
+    /**
+     * Determines status of the progressbar where
+     * progressFilled={0.1} => 10% filled progressbar
+     */
+    progressFilled?: 0 | 0.1 | 0.2 | 0.3 | 0.4 | 0.5 | 0.6 | 0.7 | 0.8 | 0.9 | 1;
+    /** Determines the type of LoadingState whether Absolute or Inline*/
+    type?: 'absolute' | 'inline';
+    /** Determines the size of LoadingState whether small , medium, or large */
+    size?: 'small' | 'medium' | 'large';
+    /** Determines the variant of LoadingState whether static or progress */
+    variant?: 'static' | 'progress';
+    /** Determines whether the LoadingState is open or not */
+    open?: boolean;
+    /** Toggles the LoadingState between open and closed */
+    isLoading?: () => any;
+    /** Starts the LoadingState Progress variant */
+    move?: () => any;
+}
+
+/**
+ * Loader that will appear based on the value of it's open prop. Also known as dialogue.
+ *
+ * @return LoadingState component
+ */
+export const LoadingState = ({
+    progressFilled = 0,
+    className,
+    type = 'absolute',
+    size = 'medium',
+    variant = 'static',
+    children,
+    style,
+    open = false,
+    isLoading,
+    move,
+    ...props
+}: Props): JSX.Element => {
+    // usestate variables
+    const [display, toggleDisplay] = useState(open);
+    const [effect, toggleEffect] = useState(open);
+
+    // Keeping track of open call
+    useEffect(() => {
+        if (open !== display) {
+            if (display) {
+                toggleEffect(false);
+                setTimeout(() => toggleDisplay(false), 300);
+            } else {
+                toggleDisplay(true);
+                setTimeout(() => toggleEffect(true), 100);
+            }
+        }
+    }, [open]);
+
+    /**
+     * Renders the LoadingState and all of its children formatted as intended
+     *
+     * @return formatted LoadingState component
+     */
+    const getVariant = (): string => {
+        // determine custom variant
+        let customVariant = '';
+        // check how much a progressbar is filled where 0 is 0% and 1 is 100%
+        if (progressFilled == 0) customVariant += 'zero-progressFilled ';
+        if (progressFilled == 0.1) customVariant += 'ten-progressFilled ';
+        if (progressFilled == 0.2) customVariant += 'twenty-progressFilled ';
+        if (progressFilled == 0.3) customVariant += 'thirty-progressFilled ';
+        if (progressFilled == 0.4) customVariant += 'fourty-progressFilled ';
+        if (progressFilled == 0.5) customVariant += 'fifty-progressFilled ';
+        if (progressFilled == 0.6) customVariant += 'sixty-progressFilled ';
+        if (progressFilled == 0.7) customVariant += 'seventy-progressFilled ';
+        if (progressFilled == 0.8) customVariant += 'eighty-progressFilled ';
+        if (progressFilled == 0.9) customVariant += 'ninty-progressFilled ';
+        if (progressFilled == 1) customVariant += 'filled-progressFilled ';
+        return customVariant;
+    };
+    /**
+     * Renders the LoadingState and all of its children formatted as intended
+     * @return formatted LoadingState component
+     */
+    const renderLoadingState = (): ReactNode => {
+        if (variant == 'progress') {
+            return (
+                <div
+                    {...props}
+                    className={`apollo-component-library-loadingstate-component-progressbar
+                    ${type}
+                    ${getVariant()}
+                    ${move ? '' : 'progress'}`}
+                ></div>
+            );
+        } else {
+            return (
+                <div
+                    {...props}
+                    className={`apollo-component-library-loadingstate-component ${type} ${size}
+                    ${getVariant()}`}
+                ></div>
+            );
+        }
+    };
+
+    if (variant == 'progress') {
+        return (
+            <React.Fragment>
+                {display ? (
+                    <div
+                        style={{ opacity: effect ? 1 : 0 }}
+                        className="apollo-component-library-loadingstate-component-container"
+                    >
+                        <div>
+                            {renderLoadingState()}
+                            <div onClick={move} />
+                        </div>
+                    </div>
+                ) : null}
+            </React.Fragment>
+        );
+    } else {
+        if (type == 'inline') {
+            return (
+                <React.Fragment>
+                    {display ? (
+                        <span style={{ display: 'inline-block' }}>{renderLoadingState()}</span>
+                    ) : null}
+                </React.Fragment>
+            );
+        } else {
+            return (
+                <React.Fragment>
+                    {display ? (
+                        <div
+                            style={{ opacity: effect ? 1 : 0 }}
+                            className="apollo-component-library-loadingstate-component-container"
+                        >
+                            <div>
+                                {renderLoadingState()}
+                                <div
+                                    onClick={isLoading}
+                                    className={`
+                                    apollo-component-library-loadingstate-component-backdrop
+                                ${type}`}
+                                />
+                            </div>
+                        </div>
+                    ) : null}
+                </React.Fragment>
+            );
+        }
+    }
+};
---
+++ stories/LoadingState.stories.tsx
@@ -0,0 +1,151 @@
+import React, { useState } from 'react';
+import { Story, Meta } from '@storybook/react';
+import { LoadingState, Props } from '../src/components/LoadingState/LoadingState';
+import { Button, Text } from '../src';
+
+const meta: Meta = {
+    title: 'Layout/LoadingState',
+    component: LoadingState,
+};
+
+export default meta;
+
+/**
+ * Template LoadingState
+ *
+ * @param args storybook arguments
+ * @return template LoadingState component
+ */
+const Template: Story<Props> = (args) => {
+    const [open, isLoading] = useState(false);
+
+    return (
+        <React.Fragment>
+            <Button onClick={() => isLoading(!open)}>Submit</Button>
+            <LoadingState open={open} isLoading={() => isLoading(!open)} {...args}></LoadingState>
+        </React.Fragment>
+    );
+};
+
+export const Default = Template.bind({});
+
+/**
+ * Template Inline LoadingState
+ *
+ * @param args storybook arguments
+ * @return template inline LoadingState component
+ */
+export const AnotherExample: Story<Props> = (args) => {
+    const [open, isLoading] = useState(false);
+
+    return (
+        <React.Fragment>
+            <Text style={{ display: 'inline-block' }} onClick={() => isLoading(!open)}>
+                Inline Loading shown here
+            </Text>
+            <LoadingState
+                open={open}
+                isLoading={() => isLoading(!open)}
+                type="inline"
+                size="small"
+                variant="static"
+                progressFilled={0}
+            ></LoadingState>
+        </React.Fragment>
+    );
+};
+
+/**
+ * Template Large LoadingState
+ *
+ * @param args storybook arguments
+ * @return template large LoadingState component
+ */
+export const LargeSize: Story<Props> = (args) => {
+    const [open, isLoading] = useState(false);
+
+    return (
+        <React.Fragment>
+            <Button onClick={() => isLoading(!open)}>Submit</Button>
+            <LoadingState
+                open={open}
+                isLoading={() => isLoading(!open)}
+                type="absolute"
+                size="large"
+                variant="static"
+                progressFilled={0}
+            ></LoadingState>
+        </React.Fragment>
+    );
+};
+
+/**
+ * Template Medium LoadingState
+ *
+ * @param args storybook arguments
+ * @return template medium LoadingState component
+ */
+export const MediumSize: Story<Props> = (args) => {
+    const [open, isLoading] = useState(false);
+
+    return (
+        <React.Fragment>
+            <Button onClick={() => isLoading(!open)}>Submit</Button>
+            <LoadingState
+                open={open}
+                isLoading={() => isLoading(!open)}
+                type="absolute"
+                size="medium"
+                variant="static"
+                progressFilled={0}
+            ></LoadingState>
+        </React.Fragment>
+    );
+};
+
+/**
+ * Template Small LoadingState
+ *
+ * @param args storybook arguments
+ * @return template small LoadingState component
+ */
+export const SmallSize: Story<Props> = (args) => {
+    const [open, isLoading] = useState(false);
+
+    return (
+        <React.Fragment>
+            <Button onClick={() => isLoading(!open)}>Submit</Button>
+            <LoadingState
+                open={open}
+                isLoading={() => isLoading(!open)}
+                type="absolute"
+                size="small"
+                variant="static"
+                progressFilled={0}
+            ></LoadingState>
+        </React.Fragment>
+    );
+};
+
+/**
+ * Template Progress LoadingState
+ *
+ * @param args storybook arguments
+ * @return template progress LoadingState component
+ */
+export const ProgressVarient: Story<Props> = (args) => {
+    const [open, isLoading] = useState(false);
+
+    return (
+        <React.Fragment>
+            <Button onClick={() => isLoading(!open)}>Submit</Button>
+            <LoadingState
+                open={open}
+                isLoading={() => isLoading(!open)}
+                type="absolute"
+                variant="progress"
+                progressFilled={1}
+            ></LoadingState>
+        </React.Fragment>
+    );
+};
