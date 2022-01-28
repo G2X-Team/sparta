75a6ed0cc856fcf99691815a4b37a3cf75945ab0
ahmed <ahmedreza80@gmail.com>
2022-01-29 04:08:39 +0530

    made test working

--- test/LoadingState.test.tsx
+++ test/LoadingState.test.tsx
@@ -1,35 +1,40 @@
 import React from 'react';
-import { screen, render, waitFor } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 
-import { LoadingState, Text } from '../src';
+import { LoadingState } from '../src';
 
 describe('LoadingState', () => {
     it('renders correctly', () => {
         // given
-        render(<LoadingState>Hello World!</LoadingState>);
+        render(<LoadingState loading />);
+        const loading = document.querySelector('[aria-busy="true"]');
 
         // when then
-        expect(screen.queryByText(/hello world!/i)).not.toBeInTheDocument();
+        expect(loading).toBeTruthy();
+    });
+
+    it('will not render', () => {
+        // given
+        render(<LoadingState loading />);
+        const loading = document.querySelector('[aria-busy="false"]');
+
+        // when then
+        expect(loading).not.toBeTruthy();
     });
 
     it('will render the progressbar type', () => {
         // given
-        render(<LoadingState size="large" type="progress" progress={0.5}></LoadingState>);
+        render(<LoadingState loading size="large" type="progress" progress={0.5} />);
         // when then
-        waitFor(() => expect(screen.getByRole('progressbar')).toBeInTheDocument());
+        expect(screen.getByRole('progressbar')).toBeInTheDocument();
     });
 
     it('will render the spinner type', () => {
         // given
-        render(
-            <Text>
-                <LoadingState size="large" type="spinner">
-                    Hello World
-                </LoadingState>
-            </Text>
-        );
+        render(<LoadingState loading size="large" type="spinner" />);
+        const loading = document.querySelector('[aria-busy="true"]');
+
         // when then
-        const label = screen.queryByText(/hello world/i);
-        waitFor(() => expect(label).toHaveAttribute('aria-busy', 'true'));
+        expect(loading).toBeTruthy();
     });
 });
