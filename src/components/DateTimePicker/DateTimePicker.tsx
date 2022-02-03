147c0c7851ce7ed8ea07e6194c4851cd9416abb4
ahmed <ahmedreza80@gmail.com>
2022-02-04 00:59:44 +0530

    component: created date picker component
    Implemented the react-datepicker library for its use in Apollo. This
    gives Apollo a well tested simplified version of a DateTimePicker.

---
+++ src/components/DateTimePicker/DateTimePicker.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import type { FC } from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import './DateTimePicker.css';
+
+export interface Props {
+    /**
+     * Determines format of date to be displayed
+     * tag i.e. format=dd/MM/yyyy => 24/12/2020
+     */
+    format?: 'dd/MM/yyyy' | 'mm/dd/yyyy' | 'yyyy/MM/dd' | 'yyyy/dd/MM';
+    /** on change method updates the date input field with selected date */
+    onChange?: (date: Date | null) => void;
+    /**  Determine the type of dropdown of year and month selecter */
+    mode?: 'select' | 'scroll';
+    /** Placeholder for no date is selected */
+    placeholder?: string;
+    /** Date to be selected */
+    selectedDate?: Date;
+}
+/**
+ * Component that serves as an datepicker input field to let the user select Date
+ *
+ * @return DateTimePicker component
+ */
+export const DateTimePicker: FC<Props> = ({
+    format = 'dd/MM/yyyy',
+    mode = 'select',
+    placeholder = 'Click to add a date',
+    selectedDate = new Date(),
+    onChange,
+}) => {
+    const [startDate, setStartDate] = useState<Date | null>(selectedDate);
+    // eslint-disable-next-line require-jsdoc
+    const newOnChange = (selectedDate: React.SetStateAction<Date | null>) => {
+        if (onChange) onChange(startDate);
+        setStartDate(selectedDate);
+    };
+    return (
+        <DatePicker
+            className="apollo-component-library-date-picker-component"
+            placeholderText={placeholder}
+            dateFormat={format}
+            selected={startDate}
+            onChange={newOnChange}
+            dropdownMode={mode}
+            showMonthDropdown
+            showYearDropdown
+            adjustDateOnChange
+        />
+    );
+};
---
+++ test/DateTimePicker.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { Default as DateTimePicker } from '../stories/DateTimePicker.stories';
+
+describe('DateTimePicker', () => {
+    it('renders correctly', () => {
+        // given
+        render(<DateTimePicker placeholder="Hello World!" />);
+
+        // then
+        expect(screen.getByPlaceholderText(/hello world!/i)).toBeInTheDocument();
+    });
+
+    it('Should not allow year input lower than 1900', () => {
+        // given
+        render(<DateTimePicker />);
+
+        // when
+        const year = document.querySelector('[value="1889"]');
+
+        // then
+        expect(year).not.toBeTruthy();
+    });
+
+    it('Should change the date', () => {
+        // given
+        const onChange: jest.Mock<any, any> = jest.fn();
+        render(<DateTimePicker placeholder="Hello World!" onChange={onChange}></DateTimePicker>);
+
+        const dateTimePickerDiv: HTMLElement = screen.getByPlaceholderText(/hello world!/i);
+
+        // when
+        userEvent.click(dateTimePickerDiv);
+        const NewDateLabel: HTMLElement = screen.getByLabelText(
+            'Choose Wednesday, March 2nd, 2022'
+        );
+        userEvent.click(NewDateLabel);
+
+        // then
+        expect(onChange).toHaveBeenCalled();
+    });
+});
