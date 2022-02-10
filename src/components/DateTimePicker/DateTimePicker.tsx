2c813f03b34b5aa1a268b12d6360f347c653fe81
ahmed <ahmedreza80@gmail.com>
2022-02-11 03:04:44 +0530

    Update

--- src/components/DateTimePicker/DateTimePicker.tsx
+++ src/components/DateTimePicker/DateTimePicker.tsx
@@ -1,29 +1,57 @@
-import type { FC } from 'react';
+import type { HTMLAttributes, FC } from 'react';
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 import './DateTimePicker.css';
 
+export interface Props extends Omit<HTMLAttributes<HTMLFormElement>, 'onChange'> {
+    /**
+     * Determines format of date to be displayed
+     * tag i.e. format=dd/MM/yyyy => 24/12/2020
+     */
+    format?:
+        | 'dd/MM/yyyy'
+        | 'mm/dd/yyyy'
+        | 'yyyy/MM/dd'
+        | 'yyyy/dd/MM'
+        | 'dd/MM/yyyy'
+        | 'mm/dd/yyyy'
+        | 'yyyy/MM/dd'
+        | 'yyyy/dd/MM';
+    // on change method updates the date input field with selected date
+    onChange?: (value: { [input: string]: string | string[] | boolean }) => void;
+    // Determine the type of dropdown of year and month selecter
+    mode?: 'select' | 'scroll';
+    // Placeholder for no date is selected
+    placeholder?: string;
+    // Date to be selected
+    selectedDate?: Date;
+}
+
 /**
- * Component that serves as an hr for ease of templating
+ * Component that serves as an datepicker input field to let the user select Date
  *
  * @return DateTimePicker component
  */
-export const DateTimePicker: FC = () => {
-    const [startDate, setStartDate] = useState<Date | null>(new Date());
+export const DateTimePicker: FC<Props> = ({
+    format = 'dd/MM/yyyy',
+    mode = 'select',
+    placeholder = 'Click to add a date',
+    selectedDate = new Date(),
+}) => {
+    const [startDate, setStartDate] = useState<Date | null>(selectedDate);
     return (
-        <>
-            <DatePicker
-                wrapperClassName="apollo-component-library-date-picker-component"
-                dateFormat="dd/MM/yyyy"
-                selected={startDate}
-                onChange={(date) => date && setStartDate(date)}
-                dropdownMode="select"
-                showMonthDropdown
-                showYearDropdown
-                adjustDateOnChange
-            />
-        </>
+        <DatePicker
+            className={`apollo-component-library-date-picker-component`}
+            placeholderText={`${placeholder}`}
+            dateFormat={`${format}`}
+            selected={startDate}
+            onChange={(date) => date && setStartDate(date)}
+            dropdownMode={`${mode}`}
+            showMonthDropdown
+            showYearDropdown
+            adjustDateOnChange
+        />
     );
 };
--- test/DateTimePicker.test.tsx
+++ test/DateTimePicker.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 
 import { Default as DateTimePicker } from '../stories/DateTimePicker.stories';
 
 describe('DateTimePicker', () => {
+    it('renders correctly', () => {
+        // given
+        render(<DateTimePicker placeholder="Hello World!" />);
+
+        // when then
+        expect(screen.getByPlaceholderText(/hello world!/i)).toBeInTheDocument();
+    });
+
     it('Should not allow year input lower than 1900', () => {
         // given
         render(<DateTimePicker />);
