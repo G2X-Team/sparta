dfbfe2c68369070ad5e700e7e28c94ea568c69a8
ahmed <ahmedreza80@gmail.com>
2022-02-15 00:13:28 +0530

    Finally got the test right

--- test/DateTimePicker.test.tsx
+++ test/DateTimePicker.test.tsx
@@ -9,31 +9,36 @@ describe('DateTimePicker', () => {
         // given
         render(<DateTimePicker placeholder="Hello World!" />);
 
-        // when then
+        // then
         expect(screen.getByPlaceholderText(/hello world!/i)).toBeInTheDocument();
     });
 
     it('Should not allow year input lower than 1900', () => {
         // given
         render(<DateTimePicker />);
+
+        // when
         const year = document.querySelector('[value="1889"]');
 
-        // when then
+        // then
         expect(year).not.toBeTruthy();
     });
 
     it('Should change the date', () => {
         // given
-        const onChange: jest.Mock<any, any> = jest.fn();
-        const selectedDate = '25/02/2022';
-        render(<DateTimePicker placeholder="Hello World!" onChange={onChange}></DateTimePicker>);
+        render(<DateTimePicker placeholder="Hello World!"></DateTimePicker>);
 
-        const checkbox: HTMLElement = screen.getByPlaceholderText(/hello world!/i);
+        const dateTimePickerDiv: HTMLElement = screen.getByPlaceholderText(/hello world!/i);
 
         // when
-        userEvent.click(checkbox);
+        userEvent.click(dateTimePickerDiv);
+        const getByLabelText: HTMLElement = screen.getByLabelText(
+            'Choose Friday, February 25th, 2022'
+        );
+        userEvent.click(getByLabelText);
+        const changedDate = document.querySelector('[value="25/02/2022"]');
 
         // then
-        expect(onChange).not.toHaveBeenLastCalledWith(selectedDate);
+        expect(changedDate).toBeTruthy();
     });
 });
