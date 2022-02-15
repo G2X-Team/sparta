0e83c4e5fcb79dffff1432a6fe8d01b031d70dc8
ahmed <ahmedreza80@gmail.com>
2022-02-16 01:43:56 +0530

    Added Table To index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,7 @@ export { Icon } from './components/Icon/Icon';
 export { Alert } from './components/Alert/Alert';
 export { LoadingState } from './components/LoadingState/LoadingState';
 export { DateTimePicker } from './components/DateTimePicker/DateTimePicker';
+export { Table } from './components/Table/Table';
 export { Label } from './components/Label/Label';
 export { Form } from './components/Form/Form';
 export { NavigationBar } from './components/NavigationBar/NavigationBar';
