5b4d1b7d17d11f838ded5c5602cac5813dd1d861
ahmed <ahmedreza80@gmail.com>
2022-02-26 00:35:44 +0530

    update

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -115,7 +115,7 @@ export const Table: FC<Props> = ({
                 >
                     <thead className="header">
                         <tr>
-                            {colNames.map((headerItem: any, index: number) => {
+                            {colNames.map((headerItem: any, index: any) => {
                                 <th key={index}>
                                     <span
                                         style={{
