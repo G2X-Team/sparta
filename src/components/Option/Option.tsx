e0ddc60704e90542e9f571b4a95c378a518ba2cb
Eddie Lopez <ejlopez00@gmail.com>
2022-03-08 14:28:14 -0500

    components: dropdown is compliant

--- src/components/Option/Option.tsx
+++ src/components/Option/Option.tsx
@@ -1,5 +1,5 @@
-import type { HTMLAttributes, FC } from 'react';
-import React from 'react';
+import { HTMLAttributes, FC, forwardRef, ForwardedRef } from 'react';
+import React, { useEffect } from 'react';
 import './Option.css';
 
 import Overload from '../../interfaces/Overload';
@@ -11,6 +11,8 @@ export interface Props extends Overload<HTMLAttributes<HTMLElement>> {
     children: string;
     /** Can have onClick callback method */
     onClick?: () => void;
+    /** Allows for forward ref */
+    ref?: ForwardedRef<HTMLLIElement>;
 }
 
 /**
@@ -18,10 +20,39 @@ export interface Props extends Overload<HTMLAttributes<HTMLElement>> {
  *
  * @return Option component
  */
-export const Option: FC<Props> = ({ children, parentProps, className = '', ...props }) => {
+// eslint-disable-next-line react/display-name
+export const Option: FC<Props> = forwardRef(function Option(
+    { children, parentProps, className, onClick, ...props }: Props,
+    ref
+) {
+    useEffect(() => {
+        if (!onClick) return;
+
+        /**
+         * Will execute the onClick method on enter
+         *
+         * @param event keyobard event
+         */
+        const onEnter = (event: KeyboardEvent): void => {
+            if (event.key === 'Enter') {
+                onClick && onClick();
+            }
+        };
+
+        window.addEventListener('keydown', onEnter);
+        return () => window.removeEventListener('keydown', onEnter);
+    }, []);
+
     return (
-        <div {...props} className={`apollo-component-library-option-component ${className}`}>
+        <li
+            {...props}
+            tabIndex={0}
+            ref={ref}
+            onClick={onClick}
+            role="option"
+            className={`apollo-component-library-option-component ${className}`}
+        >
             <Text margins={false}>{children}</Text>
-        </div>
+        </li>
     );
-};
+});
