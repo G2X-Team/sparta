4850b65c122639abe5b1ac1c383d872de99e3a6f
ahmed <ahmedreza80@gmail.com>
2022-04-02 02:58:56 +0530

    component: fixed a bug in test of DateTimePicker
    On the test to change the date we were
    trying to choose a date by label
    text of 22nd march, but once the
    month changed the test started failing.
    Fixed that by using getByText.

--- test/DateTimePicker.test.tsx
+++ test/DateTimePicker.test.tsx
@@ -33,9 +33,7 @@ describe('DateTimePicker', () => {
 
         // when
         userEvent.click(dateTimePickerDiv);
-        const NewDateLabel: HTMLElement = screen.getByLabelText(
-            'Choose Wednesday, March 2nd, 2022'
-        );
+        const NewDateLabel: HTMLElement = screen.getByText(12);
         userEvent.click(NewDateLabel);
 
         // then
