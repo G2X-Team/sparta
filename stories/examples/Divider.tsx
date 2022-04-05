dfd0304a551b5c5cc97b496a3ccea4f113fa5553
ahmed <ahmedreza80@gmail.com>
2022-04-05 20:54:24 +0530

    component: updated divider documentation
    Replaced Divider.stories.tsx with Divider.stories.mdx.
    Added a detailed documentation
    of the Divider component

---
+++ stories/examples/Divider.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import type { SampleStory } from './util';
+import { Divider } from '../../src';
+
+export const StandardDivider: SampleStory = (args) => <Divider {...args} />;
+
+export const DividerStyling: SampleStory = (args) => <Divider {...args} />;
