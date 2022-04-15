3a00fd227204b6ac54e5bf019ebf6d3485029189
ahmed <ahmedreza80@gmail.com>
2022-04-16 02:06:14 +0530

    component: added documentation of Table component
    Created Table.stories.mdx and Table.tsx
    files for the in depth documentation of
    the table component and all it's prop.

---
+++ stories/examples/Table.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import type { SampleStory } from './util';
+import { Table } from '../../src';
+
+export const StandardTable: SampleStory = (args) => {
+    return <Table {...args} />;
+};
+
+export const TableWithPagination: SampleStory = (args) => {
+    return <Table {...args} />;
+};
+
+export const CustomizedTable: SampleStory = (args) => {
+    return <Table {...args} />;
+};
