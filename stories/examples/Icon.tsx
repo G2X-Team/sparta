98cc62bdb55fe3a8ac216ab922bd5da12f64d37b
ahmed <ahmedreza80@gmail.com>
2022-04-25 20:27:28 +0530

    component: added documentation for Icon component
    Added in depth documentation for the Icon
    component with all its props and functionality

---
+++ stories/examples/Icon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import type { SampleStory } from './util';
+import { Icon } from '../../src';
+
+export const StandardIcon: SampleStory = (args) => <Icon {...args} />;
+
+export const IconStyling: SampleStory = (args) => <Icon {...args} />;
+
+export const FunctionalIcon: SampleStory = (args) => <Icon {...args} />;
