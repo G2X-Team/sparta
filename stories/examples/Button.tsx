fa6f5a7a1394d9fe8c42c1a31b5109cc415d5b28
Eddie Lopez <ejlopez00@gmail.com>
2022-08-16 15:31:42 -0400

    component: added loading state to button
    This commit was dedicated to creating a loading state for the `Button`
    component which is toggleable via the `loading` prop. When toggled, the
    button will be disabled and carry the `aria-busy` prop to let users with
    screen readers know that the button's click triggered a loading process.

---
+++ stories/examples/Button.tsx
@@ -0,0 +1,25 @@
+import React, { useState, useRef } from 'react';
+import type { SampleStory } from './util';
+import { Button } from '../../src';
+
+export const StandardButton: SampleStory = (args) => <Button {...args} />;
+
+export const ButtonRefs: SampleStory = (args) => {
+    const ref = useRef(null);
+
+    return (
+        <Button ref={ref} onClick={() => window.alert(`button ref: ${ref.current}`)}>
+            Click me
+        </Button>
+    );
+};
+
+export const LoadingButton: SampleStory = (args) => {
+    const [loading, setLoading] = useState(false);
+
+    return (
+        <Button onClick={() => setLoading(!loading)} loading={loading}>
+            Click me
+        </Button>
+    );
+};
--- test/Button.test.tsx
+++ test/Button.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { axe, toHaveNoViolations } from 'jest-axe';
+expect.extend(toHaveNoViolations);
 
 import { Button } from '../src';
 
@@ -11,6 +13,68 @@ describe('Button', () => {
 
         // when then
         expect(screen.getByText(/hello world!/i)).toBeInTheDocument();
+    });
+
+    it('complies with WCAG 2.0 AA', async () => {
+        // given
+        const { container: defaultButton } = render(<Button>Hello World!</Button>);
+        const { container: loadingButton } = render(<Button loading>Hello World!</Button>);
+        const { container: disabledButton } = render(<Button disabled>Hello World!</Button>);
+
+        // when
+        const results: any[] = [];
+        results.push(await axe(defaultButton));
+        results.push(await axe(loadingButton));
+        results.push(await axe(disabledButton));
+
+        // then
+        results.forEach((result) => {
+            expect(result).toHaveNoViolations();
+        });
+    });
+
+    it('will not be clickable when disabled', () => {
+        // given
+        const onClick: jest.Mock<any, any> = jest.fn();
+        render(
+            <Button disabled onClick={onClick}>
+                Hello World!
+            </Button>
+        );
+        const button = screen.getByRole('button');
+
+        // when
+        userEvent.click(button);
+
+        // then
+        expect(button).toBeDisabled();
+        expect(onClick).not.toHaveBeenCalled();
+    });
+
+    it('renders correctly with loading', () => {
+        // given
+        render(<Button loading>Hello World!</Button>);
+
+        // when then
+        expect(screen.getByRole('status')).toBeInTheDocument();
+        expect(screen.queryByText(/hello world!/i)).not.toBeInTheDocument();
+    });
+
+    it('will not be clickable when loading', () => {
+        // given
+        const onClick: jest.Mock<any, any> = jest.fn();
+        render(
+            <Button loading onClick={onClick}>
+                Hello World!
+            </Button>
+        );
+        const button = screen.getByRole('button');
+
+        // when
+        userEvent.click(button);
+
+        // then
+        expect(onClick).not.toHaveBeenCalled();
     });
 
     it('will call onClick call back', () => {
